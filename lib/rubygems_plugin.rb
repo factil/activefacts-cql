16e8698995a58a90f45ea6f01b95dd16c555b3f9
Graeme Port <graeme@port-nason.com>
2017-01-09 16:04:56 +1100

    Include parser .rb files in git and gem

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,12 +1,15 @@
-Gem.post_install do |installer|
-  if installer.spec.name == 'activefacts-cql'
-    pattern = installer.dir + '/lib/activefacts/cql/**/*.treetop'
-    files = Dir[pattern]
-    # Hopefully this quoting will work where there are spaces in filenames, and even maybe on Windows?
-    cmd = "tt '#{files*"' '"}'"
-    puts "Compiling Treetop grammars:"
-    puts cmd
-    system cmd
-    puts 'For more information on ActiveFacts, see http://dataconstellation.com/ActiveFacts/'
-  end
-end
+#
+# Ruby files are now included in the gem package
+#
+# Gem.post_install do |installer|
+#   if installer.spec.name == 'activefacts-cql'
+#     pattern = installer.dir + '/lib/activefacts/cql/**/*.treetop'
+#     files = Dir[pattern]
+#     # Hopefully this quoting will work where there are spaces in filenames, and even maybe on Windows?
+#     cmd = "tt '#{files*"' '"}'"
+#     puts "Compiling Treetop grammars:"
+#     puts cmd
+#     system cmd
+#     puts 'For more information on ActiveFacts, see http://dataconstellation.com/ActiveFacts/'
+#   end
+# end
