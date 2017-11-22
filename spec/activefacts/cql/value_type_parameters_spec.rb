d43500031a0e7287bd3b443e53803cca0f5c6dfe
Clifford Heath <clifford.heath@gmail.com>
2017-11-22 17:39:09 +1100

    Value Type Parameter Restrictions may now be ranges

--- spec/activefacts/cql/value_type_parameters_spec.rb
+++ spec/activefacts/cql/value_type_parameters_spec.rb
@@ -25,7 +25,7 @@ def compile cql
     vocabulary.finalise
     vocabulary.constellation.ValueTypeParameter.values.map(&:describe) * "\n" + "\n" +
     vocabulary.constellation.ValueTypeParameterRestriction.values.map(&:value_type).uniq.map do |vt|
-      "#{vt.name}(#{vt.all_value_type_parameter_restriction.map{|vtpr| "#{vtpr.value_type_parameter.name}=#{vtpr.value}"}*', '})"
+      "#{vt.name}(#{vt.all_value_type_parameter_restriction.map{|vtpr| "#{vtpr.value_type_parameter.name}=#{vtpr.value_range}"}*', '})"
     end*"\n"
   rescue => e
     e.message
@@ -39,34 +39,47 @@ describe "Value Type Parameters" do
   it "Produces the expected model for vtp.cql" do
     expected = <<END
 ValueType 'Name' Parameter 'Encoding' is of type 'String'
-Company Name(Encoding='ascii')
+Company Name(Encoding='ASCII')
 Family Name(Encoding='latin1')
+Farnarkle Name(Encoding=4)
 Given Name(Encoding='utf8')
-Name(Encoding='ascii', Encoding='latin1', Encoding='utf8')
+Name(Encoding=1..4, Encoding='ASCII'..'blarf', Encoding='latin1', Encoding='utf8')
 Personal Name(Encoding='latin1', Encoding='utf8')
 END
     actual = compile(cql)
+    # puts 'v'*40; puts actual; puts '^'*40
     expect(actual.split(/\n/)).to be_like(expected.split(/\n/))
   end
 
-  it "Fails with the right error when you use a value not in the allowed set" do
+  it "Fails with the right error when you use a value not in the allowed set of strings" do
     # Changing the base restrictions disallows assignments that use the old value
-    actual = compile(cql.sub(/{'ascii'/, "{'ASCII'"))
-    expected = %q{at line 9 value 'ascii' is restricted by Name to ['ASCII', 'latin1', 'utf8']}
-    expect(actual).to be_like(expected), "Output doesn't match expected"
+    actual = compile(cql.sub(/{'ASCII'/, "{'ascii'"))
+    # puts 'v'*40; puts actual; puts '^'*40
+    expected = %q{at line 10 value 'ASCII' is restricted by Name to [1..4, 'ascii'..'blarf', 'latin1', 'utf8']}
+    expect(actual).to be_like(expected), "Output '#{actual}' doesn't match expected"
+  end
+
+  it "Fails with the right error when you use a value not in the allowed set of integers" do
+    # Changing the base restrictions disallows assignments that use the old value
+    actual = compile(cql.sub(/Encoding: 4/, "Encoding: 5"))
+    # puts 'v'*40; puts actual; puts '^'*40
+    expected = %q{at line 9 value 5 is restricted by Name to [1..4, 'ASCII'..'blarf', 'latin1', 'utf8']}
+    expect(actual).to be_like(expected), "Output '#{actual}' doesn't match expected"
   end
 
   it "Fails with the right error when you try to change a restriction" do
     # Removing the word "Company" makes it look like we're re-assigning the restrictions on Name:
     actual = compile(cql.sub(%r{Company }, ""))
-    expected = %q{at line 9 You can't change the existing restrictions on parameter Encoding of Name}
+    # puts 'v'*40; puts actual; puts '^'*40
+    expected = %q{at line 10 You can't change the existing restrictions on parameter Encoding of Name}
     expect(actual).to be_like(expected), "Output '#{actual}' doesn't match expected"
   end
 
   it "Fails with the right error when you widen a restriction on a subclass" do
     # Change the restriction on Personal Name:
     actual = compile(cql.sub(/{'latin1',/, "{'iso-8859-1',"))
-    expected = %q{at line 10 value 'iso-8859-1' is restricted by Name to ['ascii', 'latin1', 'utf8']}
+    # puts 'v'*40; puts actual; puts '^'*40
+    expected = %q{at line 11 value 'iso-8859-1' is restricted by Name to [1..4, 'ASCII'..'blarf', 'latin1', 'utf8']}
     expect(actual).to be_like(expected), "Output '#{actual}' doesn't match expected"
   end
 
