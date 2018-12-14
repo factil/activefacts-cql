01af6b733fb6e4d2321ee6ee85bd2e4bc7656ab3
Clifford Heath <clifford.heath@gmail.com>
2018-12-14 14:05:56 +1100

    Simplified input context management for parser

--- lib/activefacts/cql/parser.rb
+++ lib/activefacts/cql/parser.rb
@@ -6,18 +6,13 @@
 #
 require 'rubygems'
 require 'treetop'
+require 'delegate'
 
-# These are Treetop files, which Polyglot will compile on the fly if precompiled ones aren't found:
+# Include the Treetop files, which Polyglot will compile on the fly if precompiled ones aren't found:
 require 'activefacts/cql/parser/CQLParser'
 
-module ActiveFacts
-  module CQL
-    class Parser < CQLParser
-    end
-  end
-end
-
 class Treetop::Runtime::SyntaxNode
+  # node_type is used in colourising a parse tree
   def node_type
     terminal? ? :keyword : :composite
   end
@@ -32,204 +27,176 @@ module ActiveFacts
     end
 
     # Extend the generated parser:
-    class Parser
+    class Parser < CQLParser
       include ActiveFacts
 
-      # The Context manages some key information revealed or needed during parsing
-      # These methods are semantic predicates; if they return false this parse rule will fail.
-      class Context
-        attr_reader :term, :global_term
-        attr_reader :terms
-
-        def initialize(parser)
+      # Parser actions on SyntaxNodes have the @input available but not the parser.
+      # Delegate input, so we have a handle to the parser in actions.
+      class InputProxy < SimpleDelegator
+        attr_reader :parser
+        def initialize input, parser
+          super(input)
           @parser = parser
-          @terms = {}
-          @role_names = {}
-          @allowed_forward_terms = []
-        end
-
-        def object_type(name, kind)
-          index_name(@terms, name) && trace(:context, "new #{kind} '#{name}'")
-          true
-        end
-
-        def reset_role_names
-          trace :context, "\tresetting role names #{@role_names.keys.sort*", "}" if @role_names && @role_names.size > 0
-          @role_names = {}
-        end
-
-        def allowed_forward_terms(terms)
-          @allowed_forward_terms = terms
-        end
-
-        def new_leading_adjective_term(adj, term)
-          index_name(@role_names, "#{adj} #{term}", term) && trace(:context, "new compound term '#{adj}- #{term}'")
-          true
-        end
-
-        def new_trailing_adjective_term(adj, term)
-          index_name(@role_names, "#{term} #{adj}", term) && trace(:context, "new compound term '#{term} -#{adj}'")
-          true
-        end
-
-        def role_name(name)
-          index_name(@role_names, name) && trace(:context, "new role '#{name}'")
-          true
-        end
-
-        def term_starts?(s, context_saver)
-          @term = @global_term = nil
-
-          @term_part = s
-          @context_saver = context_saver
-          t = @terms[s] || @role_names[s] || system_term(s)
-          if t
-            # s is a prefix of the keys of t.
-            if t[s]
-              @global_term = @term = @term_part
-              @context_saver.context = {:term => @term, :global_term => @global_term }
-            end
-            trace :context, "Term #{t[s] ? "is" : "starts"} '#{@term_part}'"
-          elsif @allowed_forward_terms.include?(@term_part)
-            @term = @term_part
-            @context_saver.context = {:term => @term, :global_term => @term }
-            trace :context, "Term #{s} is an allowed forward"
-            return true
-          end
-          t
-        end
-
-        def term_continues?(s)
-          @term_part = "#{@term_part} #{s}"
-          t = @terms[@term_part]
-          r = @role_names[@term_part]
-          if t && (!r || !r[@term_part])    # Part of a term and not a complete role name
-            w = "term"
-          else
-            t = r
-            w = "role_name"
-          end
-          if t
-            trace :context, "Multi-word #{w} #{t[@term_part] ? 'ends at' : 'continues to'} #{@term_part.inspect}"
-
-            # Record the name of the full term and the underlying global term:
-            if t[@term_part]
-              @term = @term_part if t[@term_part]
-              @global_term = (t = t[@term_part]) == true ? @term_part : t
-              trace :context, "saving context #{@term}/#{@global_term}"
-              # trace :context, "@terms =\n\t#{@terms.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
-              # trace :context, "@role_names =\n\t#{@role_names.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
-              @context_saver.context = {:term => @term, :global_term => @global_term }
-            end
-          end
-          t
-        end
-
-        def term_complete?
-          return true if @allowed_forward_terms.include?(@term)
-          return true if system_term(@term)
-          result = ((t = @terms[@term] and t[@term]) or (t = @role_names[@term] and t[@term]))
-          trace :context, "term #{@term} is #{result ? '' : 'in'}complete"
-          result
-        end
-
-        def global_term_starts?(s, context_saver)
-          @term = @global_term = nil
-
-          @term_part = s
-          @context_saver = context_saver
-          t = @terms[s] || system_term(s)
-          if t
-            # s is a prefix of the keys of t.
-            if t[s]
-              @global_term = @term = @term_part
-              @context_saver.context = {:term => @term, :global_term => @global_term }
-            end
-            trace :context, "Term #{t[s] ? "is" : "starts"} '#{@term_part}'"
-          elsif @allowed_forward_terms.include?(@term_part)
-            @term = @term_part
-            @context_saver.context = {:term => @term, :global_term => @term }
-            trace :context, "Term #{s} is an allowed forward"
-            return true
-          end
-          t
-        end
-
-        def global_term_continues?(s)
-          @term_part = "#{@term_part} #{s}"
-          t = @terms[@term_part]
-          if t
-            trace :context, "Multi-word term #{t[@term_part] ? 'ends at' : 'continues to'} #{@term_part.inspect}"
-
-            # Record the name of the full term and the underlying global term:
-            if t[@term_part]
-              @term = @term_part if t[@term_part]
-              @global_term = (t = t[@term_part]) == true ? @term_part : t
-              trace :context, "saving context #{@term}/#{@global_term}"
-              # trace :context, "@terms =\n\t#{@terms.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
-              @context_saver.context = {:term => @term, :global_term => @global_term }
-            end
-          end
-          t
-        end
-
-        def system_term(s)
-          false
-        end
-
-        def unit? s
-          @parser.unit? s
-        end
-
-      private
-        # Index the name by all prefixes
-        def index_name(index, name, value = true)
-          added = false
-          words = name.split(/\s+/)
-          words.inject("") do |n, w|
-            # Index all prefixes up to the full term
-            n = n.empty? ? w : "#{n} #{w}"
-            index[n] ||= {}
-            added = true unless index[n][name]
-            index[n][name] = value    # Save all possible completions of this prefix
-            n
-          end
-          added
         end
       end
 
-      class InputProxy
-        attr_reader :context, :parser
+      # This contains a hash of the terms we know about, including all whole-word prefixes
+      attr_reader :terms
 
-        def initialize(input, context, parser)
-          @input = input
-          @context = context
-          @parser = parser
-        end
+      # Information needed while parsing a term:
+      attr_reader :term, :global_term
 
-        def length
-          @input.length
-        end
+      def initialize
+        @terms = {}
+        @role_names = {}
+        @allowed_forward_terms = []
+      end
 
-        def size
-          length
-        end
+      def new_object_type_name(name, kind)
+        index_name(@terms, name) && trace(:terms, "new #{kind} '#{name}'")
+        true
+      end
 
-        def [](*a)
-          @input[*a]
-        end
+      def reset_role_names
+        trace :terms, "\tresetting role names #{@role_names.keys.sort*", "}" if @role_names && @role_names.size > 0
+        @role_names = {}
+      end
 
-        def index(*a)
-          @input.index(*a)
-        end
+      def allowed_forward_terms(terms)
+        @allowed_forward_terms = terms
+      end
 
-        def line_of(x)
-          @input.line_of(x)
-        end
+      def new_leading_adjective_term(adj, term)
+        index_name(@role_names, "#{adj} #{term}", term) && trace(:terms, "new compound term '#{adj}- #{term}'")
+        true
+      end
 
-        def column_of(x)
-          @input.column_of(x)
+      def new_trailing_adjective_term(adj, term)
+        index_name(@role_names, "#{term} #{adj}", term) && trace(:terms, "new compound term '#{term} -#{adj}'")
+        true
+      end
+
+      def new_role_name(name)
+        index_name(@role_names, name) && trace(:terms, "new role '#{name}'")
+        true
+      end
+
+      # The methods named ending in ? are semantic predicates; if they return false this parse rule will fail.
+
+      def term_starts?(s, context_saver)
+        @term = @global_term = nil
+
+        @term_part = s
+        @context_saver = context_saver
+        t = @terms[s] || @role_names[s] || system_term(s)
+        if t
+          # s is a prefix of the keys of t.
+          if t[s]
+            @global_term = @term = @term_part
+            @context_saver.context = {:term => @term, :global_term => @global_term }
+          end
+          trace :terms, "Term #{t[s] ? "is" : "starts"} '#{@term_part}'"
+        elsif @allowed_forward_terms.include?(@term_part)
+          @term = @term_part
+          @context_saver.context = {:term => @term, :global_term => @term }
+          trace :terms, "Term #{s} is an allowed forward"
+          return true
         end
+        t
+      end
+
+      def term_continues?(s)
+        @term_part = "#{@term_part} #{s}"
+        t = @terms[@term_part]
+        r = @role_names[@term_part]
+        if t && (!r || !r[@term_part])    # Part of a term and not a complete role name
+          w = "term"
+        else
+          t = r
+          w = "role_name"
+        end
+        if t
+          trace :terms, "Multi-word #{w} #{t[@term_part] ? 'ends at' : 'continues to'} #{@term_part.inspect}"
+
+          # Record the name of the full term and the underlying global term:
+          if t[@term_part]
+            @term = @term_part if t[@term_part]
+            @global_term = (t = t[@term_part]) == true ? @term_part : t
+            trace :terms, "saving context #{@term}/#{@global_term}"
+            # trace :terms, "@terms =\n\t#{@terms.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
+            # trace :terms, "@role_names =\n\t#{@role_names.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
+            @context_saver.context = {:term => @term, :global_term => @global_term }
+          end
+        end
+        t
+      end
+
+      def term_complete?
+        return true if @allowed_forward_terms.include?(@term)
+        return true if system_term(@term)
+        result = ((t = @terms[@term] and t[@term]) or (t = @role_names[@term] and t[@term]))
+        trace :terms, "term #{@term} is #{result ? '' : 'in'}complete"
+        result
+      end
+
+      def global_term_starts?(s, context_saver)
+        @term = @global_term = nil
+
+        @term_part = s
+        @context_saver = context_saver
+        t = @terms[s] || system_term(s)
+        if t
+          # s is a prefix of the keys of t.
+          if t[s]
+            @global_term = @term = @term_part
+            @context_saver.context = {:term => @term, :global_term => @global_term }
+          end
+          trace :terms, "Term #{t[s] ? "is" : "starts"} '#{@term_part}'"
+        elsif @allowed_forward_terms.include?(@term_part)
+          @term = @term_part
+          @context_saver.context = {:term => @term, :global_term => @term }
+          trace :terms, "Term #{s} is an allowed forward"
+          return true
+        end
+        t
+      end
+
+      def global_term_continues?(s)
+        @term_part = "#{@term_part} #{s}"
+        t = @terms[@term_part]
+        if t
+          trace :terms, "Multi-word term #{t[@term_part] ? 'ends at' : 'continues to'} #{@term_part.inspect}"
+
+          # Record the name of the full term and the underlying global term:
+          if t[@term_part]
+            @term = @term_part if t[@term_part]
+            @global_term = (t = t[@term_part]) == true ? @term_part : t
+            trace :terms, "saving context #{@term}/#{@global_term}"
+            # trace :terms, "@terms =\n\t#{@terms.map{|k,v| "#{k} => #{v}"} * "\n\t"}"
+            @context_saver.context = {:term => @term, :global_term => @global_term }
+          end
+        end
+        t
+      end
+
+      def system_term(s)
+        # We don't define any system terms here, but an example would be "Now" - the transaction's clock.
+        false
+      end
+
+      # Index the name by all prefixes
+      def index_name(index, name, value = true)
+        added = false
+        words = name.split(/\s+/)
+        words.inject("") do |n, w|
+          # Index all prefixes up to the full term
+          n = n.empty? ? w : "#{n} #{w}"
+          index[n] ||= {}
+          added = true unless index[n][name]
+          index[n][name] = value    # Save all possible completions of this prefix
+          n
+        end
+        added
       end
 
       def allow_forward_terms_in role_list
@@ -240,11 +207,7 @@ module ActiveFacts
             role.term
           end.
           compact
-        context.allowed_forward_terms(forwards)
-      end
-
-      def context
-        @context ||= Context.new(self)
+        allowed_forward_terms(forwards)
       end
 
       def unit?(s)
@@ -253,7 +216,7 @@ module ActiveFacts
       end
 
       def parse(input, options = {})
-        input = InputProxy.new(input, context, self) unless input.respond_to?(:context)
+        input = InputProxy.new(input, self)
         super(input, options)
       end
 
@@ -265,7 +228,7 @@ module ActiveFacts
         self.consume_all_input = false
         asts = []
         begin
-          tree = parse(InputProxy.new(input, context, self), :index => @index)
+          tree = parse(InputProxy.new(input, self), :index => @index)
           unless tree 
             raise failure_reason || "not all input was understood" unless @index == input.size
             return nil  # No input, or no more input
--- spec/helpers/test_parser.rb
+++ spec/helpers/test_parser.rb
@@ -2,14 +2,9 @@
 require 'activefacts/cql/parser/Language/English'
 class TestParser < ActiveFacts::CQL::Parser
   include ActiveFacts::CQL::English
-  def context
-    @context ||= Context.new(self)
-  end     
 
-  class Context < ActiveFacts::CQL::Parser::Context
-    # Capitalised words that are otherwise undefined are treated as terms:
-    def system_term(t)
-      (first = t[0,1] and first.upcase == first) ? {t=>t} : false
-    end
+  # Capitalised words that are otherwise undefined are treated as terms:
+  def system_term(t)
+    (first = t[0,1] and first.upcase == first) ? {t=>t} : false
   end
 end
